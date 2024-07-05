02c16693171fb7a3834ac0716ceee8879d7683df
joelinfo19 <joel_zerg_pro@outlook.com>
2024-07-05 02:16:59 -0500

    update_module_public_repository

---
+++ third_module/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "dev-eks-labs-terraform-state"
+    key    = "public-repository/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.0.1"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+}
+
+variable "region" {
+  description = "The region to deploy the resources"
+  type        = string
+  default     = "us-east-2"
+}
